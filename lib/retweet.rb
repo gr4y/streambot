2fa654a30981afeb0b6fb3c1bbce762f25fd0634
Sascha Wessel <swessel@gr4yweb.de>
2010-04-13 22:56:16 +0200

    added comments

--- lib/retweet.rb
+++ lib/retweet.rb
@@ -3,11 +3,13 @@ require 'open-uri'
 
 class Retweet
   
+  # intitialize method aka constructor
   def initialize(auth)
     return if auth.nil?
     @auth = auth 
   end
   
+  # retweets the status with given id
   def retweet(id)
     url = URI.parse("http://api.twitter.com/1/statuses/retweet/#{id}.json")
     req = Net::HTTP::Post.new(url.path)
