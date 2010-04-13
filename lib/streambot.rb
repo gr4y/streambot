dd21f223af58b3aab54b107dd17e3b603a00fb9a
Sascha Wessel <swessel@gr4yweb.de>
2010-04-13 22:57:03 +0200

    added a elementary blacklist

--- lib/streambot.rb
+++ lib/streambot.rb
@@ -4,19 +4,25 @@ require 'retweet'
 
 class StreamBot
   
-  def initialize(auth, *keywords)
+  # initialize method aka constructor  
+  def initialize(auth, blacklist=nil, *keywords)
     @stream = TweetStream::Client.new(auth[:username],auth[:password])
     @retweet = Retweet.new(auth)
     @keywords=keywords.join(',')
+    @blacklist=blacklist if !blacklist.nil?
   end
   
+  # starts the bot
   def start
     @stream.track(@keywords) do |status|
-      puts "#{status.text} - #{status.id}"
-      @retweet.retweet(status.id)
+      if !@blacklist.include?(status.user)
+        #puts "#{status.text} - #{status.id}"
+        @retweet.retweet(status.id)
+      end
     end  
   end  
   
+  # stop bot
   def stop
     @stream.stop
   end
