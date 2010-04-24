2e093f506d622e0c4a0c9ad8a3ba04f695505c1f
Sascha Wessel <swessel@gr4yweb.de>
2010-04-24 19:57:36 +0200

    refactored tests

--- test/streambot/base_test.rb
+++ test/streambot/base_test.rb
@@ -1,35 +1,35 @@
 require 'rubygems'
-require 'yaml'
 require 'test/unit'
 require 'webmock'
-
+require 'yaml'
 include WebMock
 
-# The base of all tests in this projet
-# New Testcases need to look like this:
-#
-#
-# class TestName < BaseTest
-#  
-#  def setup 
-#    super
-#    # implementation goes here
-#  end
-#  
-# end
-class BaseTest < Test::Unit::TestCase
-  
-  # reading config.yml and assign all keys to instance
-  def read_config  
-    yml_config = YAML.load_file(File.dirname(__FILE__) + "/config.yml")
-    yml_config.each do |key, value| 
-      instance_variable_set("@#{key}", value)
+require 'streambot'
+
+module StreamBot
+  # The base of all tests in this projet
+  #
+  # class TestName < StreamBot::BaseTest
+  #  
+  #  def setup 
+  #    super
+  #    # implementation goes here
+  #  end
+  #  
+  # end
+  class BaseTest < Test::Unit::TestCase
+    
+    # reading config.yml and assign all keys to instance
+    def read_config  
+      yml_config = YAML.load_file(File.dirname(__FILE__) + "/config.yml")
+      yml_config.each do |key, value| 
+        instance_variable_set("@#{key}", value)
+      end
+    end
+    
+    # :nodoc:
+    def setup
+      self.read_config
     end
   end
-  
-  # :nodoc:
-  def setup
-    self.read_config
-  end
-  
 end
---
+++ test/streambot/test_retweet.rb
@@ -0,0 +1,31 @@
+require 'streambot/base_test'
+
+module StreamBot
+  # the test for testing retweet functionality
+  class TestRetweet < StreamBot::BaseTest
+    # setup the test 
+    def setup  
+      super
+      @id = 12355936428
+      @retweet = StreamBot::Retweet.new(@auth)
+      stub_request(:post, "http://#{@auth[:username]}:#{@auth[:password]}@api.twitter.com/1/statuses/retweet/#{@id}.json").to_return(:body => 'content')
+    end
+    
+    # teardown the test
+    # nil all instance variables
+    def teardown
+      @id, @retweet = nil
+    end
+    
+    # retweet 
+    def test_retweet
+      # no permission to access web!
+      WebMock.disable_net_connect!
+      # retweet the given id
+      response = @retweet.retweet(@id)
+      # assertion
+      assert_not_nil(response) 
+      assert_equal('content',response.body)    
+    end
+  end
+end
