1782ecb528337f717fd3c114dacdc96cb0bec8fe
Sascha Wessel <swessel@gr4yweb.de>
2010-05-16 13:32:42 +0200

    mocking webrequests now

--- test/streambot/test_oauth.rb
+++ test/streambot/test_oauth.rb
@@ -7,11 +7,28 @@ module StreamBot
     def setup
       WebMock.allow_net_connect!
       @oauth = StreamBot::OAuth.new
+      WebMock.disable_net_connect!
     end
 
     def test_verify_credentials
-      json = @oauth.get('/account/verify_credentials.json')
-      puts json
+      begin
+        stub_request(:get, "http://api.twitter.com/account/verify_credentials.json").to_return(:body=> "response")
+        res = @oauth.get('/account/verify_credentials.json')
+        assert_not_nil(res)
+        assert(res=="response")
+      rescue RuntimeError => e
+        puts e
+      end
+    end
+
+    def test_post_update
+      body = {:status => 'test status via oauth'}
+      stub_request(:post, "http://api.twitter.com/statuses/update.json").to_return(:body=> "response")
+      begin
+        res = @oauth.post('/statuses/update.json',body)
+        assert_not_nil(res)
+        assert(res=="response")
+      end
     end
 
     # nil all variables
