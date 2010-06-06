81f4eea390d4b348a2f8b533a02bc04216a609e1
Sascha Wessel <swessel@gr4yweb.de>
2010-06-06 13:57:25 +0200

    stubbing http request and oauth request

--- test/streambot/test_retweet.rb
+++ test/streambot/test_retweet.rb
@@ -7,10 +7,14 @@ module StreamBot
     def setup
       super
       @retweet = StreamBot::Retweet.new(@params)
-      stub_request(:post, "http://api.twitter.com/statuses/retweet/12355936428.json").to_return(:body => "response")
+      if @params['auth_type'] == "oauth"
+        stub_request(:post, "http://api.twitter.com/statuses/retweet/12355936428.json").to_return(:body => "response")
+      elsif @params['auth_type'] == "http"
+        stub_request(:post, "http://#{@params['http']['username']}:#{@params['http']['password']}@api.twitter.com/statuses/retweet/12355936428.json").to_return(:body => "response")
+      end
     end
 
-    # retweet 
+    # retweet
     def test_retweet
       # no permission to access web!
       WebMock.disable_net_connect!
@@ -25,6 +29,6 @@ module StreamBot
     # nil all instance variables
     def teardown
       @id, @retweet = nil
-    end    
+    end
   end
 end
