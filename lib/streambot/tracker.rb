17020de59b6090f51afcdfef14140e198a104c65
Sascha Wessel <swessel@gr4yweb.de>
2010-06-26 18:06:24 +0200

    moved filtering into the tracker class

--- lib/streambot/tracker.rb
+++ lib/streambot/tracker.rb
@@ -2,32 +2,52 @@ module StreamBot
   # The Tracker class that provides a start and a stop method 
   class Tracker
     # the initialization method aka the constructor
-    def initialize(params, blacklist, *keywords)
+    def initialize(params, blacklist, * keywords)
       LOG.debug("Tracker#initialize")
       http_auth = params['http']
-      @stream = TweetStream::Client.new(http_auth['username'],http_auth['password'])
+      @stream = TweetStream::Client.new(http_auth['username'], http_auth['password'])
       # initializing retweet
       @retweet = StreamBot::Retweet.new(params)
       # get string with keywords comma separated keywords 
       @keywords=keywords.join(',')
       # set blacklist array if not nil
       @blacklist=blacklist
+      if !@blacklist.nil?
+        warn "blacklisting is deprecated and will be removed with streambot 0.6.0"
+      end
     end
-    
+
     # start the bot
     def start
       LOG.debug("Tracker#start")
       # starting to track the keywords via tweetstream
       @stream.track(@keywords) do |status|
         username = status.user.screen_name
-        # if status.user is NOT in blacklist then retweet it
-        if !@blacklist.include?(username)
+        # if status.user is NOT in blacklist or filters don't match then retweet it
+        if !@blacklist.include?(username) || !match?(status)
           LOG.debug("Tracker#start - retweet ##{status.id} from @#{username}")
           @retweet.retweet(status.id)
         end
-      end  
-    end  
-    
+      end
+    end
+
+    # returns true if an filter is matching
+    def match?(status)
+      filters = params['filters']
+      filters.each do |filter|
+        @type = filter['type']
+        @value = filter['value']
+        case @type
+          when "USER" then
+            status.user.screen_name.eql?(@value)
+          when "CONTENT" then
+            status.text.include?(@value)
+          when "LANGUAGE" then
+            status.lang.eql?(@value)
+        end
+      end
+    end
+
     # stop the bot
     def stop
       @stream.stop
