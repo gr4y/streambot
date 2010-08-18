25d4582dc890912d1e5c3b937df2a06b34507c07
Sascha Wessel <swessel@gr4yweb.de>
2010-08-18 18:41:57 +0200

    filtering was completely failing. but now it works. wohooo. ^.^

--- lib/streambot/tracker.rb
+++ lib/streambot/tracker.rb
@@ -15,6 +15,7 @@ module StreamBot
       if !@blacklist.nil?
         warn "blacklisting is deprecated and will be removed with streambot 0.6.0"
       end
+      @filters = params['filters']
     end
 
     # start the bot
@@ -23,28 +24,31 @@ module StreamBot
       # starting to track the keywords via tweetstream
       @stream.track(@keywords) do |status|
         username = status.user.screen_name
+
         # if status.user is NOT in blacklist or filters don't match then retweet it
-        if !@blacklist.include?(username) || !match?(status)
-          LOG.debug("Tracker#start - retweet ##{status.id} from @#{username}")
-          @retweet.retweet(status.id)
+        if @blacklist.nil? || !@blacklist.include?(username)
+          @filters.each do |key, value|
+            if !match?(status, value)
+              LOG.debug("Tracker#start - retweet ##{status.id} from @#{username}")
+              @retweet.retweet(status.id)
+            else
+              LOG.debug("Tracker#start - filter #{key} matched!")
+            end
+          end
         end
       end
     end
 
     # returns true if an filter is matching
-    def match?(status)
-      filters = params['filters']
-      filters.each do |filter|
-        @type = filter['type']
-        @value = filter['value']
-        case @type
-          when "USER" then
-            status.user.screen_name.eql?(@value)
-          when "CONTENT" then
-            status.text.include?(@value)
-          when "LANGUAGE" then
-            status.lang.eql?(@value)
-        end
+    def match?(status, filter)
+      @type = filter["type"]
+      @value = filter["value"]
+      if @type == "USER" then
+        status.user.screen_name.eql?(@value)
+      elsif @type == "CONTENT" then
+        status.text.include?(@value)
+      elsif @type == "LANGUAGE" then
+        status.lang.eql?(@value)
       end
     end
 
