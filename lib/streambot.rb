366d7ac1a0f5727bbbf6b4a5ac043b8ee389271b
Sascha Wessel <swessel@gr4yweb.de>
2010-12-13 13:40:02 +0100

    removed tweetstream as requirement and added twistream.

--- lib/streambot.rb
+++ lib/streambot.rb
@@ -2,19 +2,21 @@ require 'rubygems'
 require 'open-uri'
 require 'net/http'
 require 'oauth'
-require 'tweetstream'
+require 'twistream'
 require 'logger'
 require 'yaml'
 
-require 'streambot/callbacks'
+require 'streambot/version'
+require 'streambot/event'
+require 'streambot/event_handler'
 require 'streambot/retweet'
 require 'streambot/handler'
 require 'streambot/oauth'
 require 'streambot/tracker'
 
-module StreamBot  
+module StreamBot
   LOG = Logger.new(STDOUT)
   LIBPATH = ::File.expand_path(::File.dirname(__FILE__)) + ::File::SEPARATOR
   PATH = ::File.dirname(LIBPATH) + ::File::SEPARATOR
-  VERSION = ::File.exist?(PATH + 'VERSION') ? ::File.read(PATH+'VERSION') : ""
+  VERSION = StreamBot::Version::STRING
 end
