73d1ee269d11ee189a0ec0ba952b4889429c4de2
Sascha Wessel <swessel@gr4yweb.de>
2011-02-11 22:58:19 +0100

    moved some code in an extra method

--- lib/streambot/event_handler.rb
+++ lib/streambot/event_handler.rb
@@ -12,20 +12,24 @@ module StreamBot
         event(name)
       end
     end
-
+    
     private
     # the real event registration
     def register_event(name)
       class_eval do
-        variable =:"@#{name}"
-        define_method(name) do
-          event = instance_variable_get(variable)
-          if event == nil
-            event = StreamBot::Event.new(name)
-            instance_variable_set(variable, event)
-          end
-          event
+        define_event_method(name);
+      end
+    end
+    
+    def define_event_method(name)
+      variable =:"@#{name}"
+      define_method(name) do
+        event = instance_variable_get(variable)
+        if event == nil
+          event = StreamBot::Event.new(name)
+          instance_variable_set(variable, event)
         end
+        event
       end
     end
   end
