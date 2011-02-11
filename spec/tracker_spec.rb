09f53183a6be17f8774c0191f72ce95465e8c3e6
Sascha Wessel <swessel@gr4yweb.de>
2011-02-11 23:00:08 +0100

    fixed tracker spec

--- spec/tracker_spec.rb
+++ spec/tracker_spec.rb
@@ -5,10 +5,10 @@ describe StreamBot::Tracker do
   before :all do
     params = YAML.load_file('spec/params.yml')
     @tracker = StreamBot::Tracker.new(params)
-    @tracker.on_error do |msg, trace|
+    @tracker.on_error.receive do |msg, trace|
       puts "#{msg}: #{trace}"
     end
-    @tracker.on_match do |status, filter_path, filter_value|
+    @tracker.on_match.receive do |status, filter_path, filter_value|
       puts "filter matched on #{filter_path} with #{filter_value} in status ##{status['id']}"
     end
   end
@@ -23,7 +23,7 @@ describe StreamBot::Tracker do
   
   describe "wait" do 
     it "should sleep for 60 seconds" do 
-      sleep 10
+      sleep 60
     end
   end
   
